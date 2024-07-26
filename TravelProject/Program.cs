e5d19213a0590f3be663b986d9b1ab841a5253f4
JayPrakash Yadav <jpyadav58105@gmail.com>
2024-07-26 15:13:13 +0545

    Initial commit

--- TravelProject/Program.cs
+++ TravelProject/Program.cs
@@ -16,7 +16,6 @@ namespace TravelProject
 
             var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.UseSwagger();
